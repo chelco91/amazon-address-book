685a3b81910b87db2f26721c70720db1b1fd2bc4
Christopher Collopy <chelco91@gmail.com>
2015-07-16 17:09:04 -0600

    wrote index.php and added css via styles.css

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Amazon Address Book</title>
+		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Tangerine">
+	</head>
+	<body>
+		<div class="container">
+			<header>
+				<h1>Amazon Address Book</h1>
+			</header>
+			<main>
+				<div class="persona">
+					<div class="top">
+						<div class="width33">
+							<img src="images/trevor-rabin.jpg" height="50px" width="80px"/>
+							<p><strong>Name: </strong>Trevor Rabin</p>
+							<p><strong>Age: </strong>35</p>
+						</div>
+						<div class="width33">
+							<p><strong>Profession: </strong>
+								Trevor is a hard working book salesman. He likes books. He saw Amazon had just the sale for him.
+							</p>
+						</div>
+						<div class="width33">
+							<p><strong>Technology: </strong>
+								Trevor utilizes lots of web applications for his business. Also he is VERY familiar with Amazon's site.
+							</p>
+						</div>
+					</div>
+					<hr />
+					<div class="bottom">
+						<div class="width33">
+							<p><strong>Attitudes and Behaviors: </strong>
+								Trevor loves to make reading suggestions to all his family and friends. He will typically send out a few books every week.
+							</p>
+						</div>
+						<div class="width33">
+							<p><strong>Frustrations and Needs: </strong>
+								Trevor has lots of trouble remembering which friend lives where. He needs someplace to store lots of different shipping addresses. Also his shipping and billing address don't match very often.
+							</p>
+						</div>
+						<div class="width33">
+							<p><strong>Goals: </strong>
+								Trevor's goal right now is to purchase his book in a timely manner allowing him to catch the discounted price.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="useCases">
+					<h2>Use Cases</h2>
+					<ol>
+						<li>User types in amazon.com into their browser.</li>
+						<li>User logs in using their email and password.</li>
+						<li>User selects products to purchase and sends them to their cart.</li>
+						<li>When the user is done shopping, they click on their shopping cart.</li>
+						<li>User then clicks "Check Out" to finalize their purchases.</li>
+						<li>User is prompted for a shipping address to send products to.</li>
+						<li>System populates order with a default shipping and billing address.</li>
+						<li>User chooses to use default values.</li>
+					</ol>
+				</div>
+			</main>
+		</div>
+	</body>
+</html>
